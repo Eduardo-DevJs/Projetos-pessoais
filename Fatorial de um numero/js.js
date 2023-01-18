e687e46de61dccf6e36f24364fcc620aff1358aa
Eduardo de Oliveira Liria <dudev2001@gmail.com>
2023-01-18 01:51:34 -0300

    Update

---
+++ Fatorial de um numero/js.js
@@ -0,0 +1,24 @@
+let numeroFator = document.querySelector('.numero')
+let fator = document.querySelector(".fator")
+let resposta = document.querySelector(".repostaFator")
+console.log(numeroFator)
+
+
+function fatorDeUmNumero(){
+  let fator = 1
+  for(let i = 1; i <= numeroFator.value; i++){
+    fator *= i
+  }
+  resposta.innerHTML = `O fatorial do ${numeroFator.value} é ${fator}`
+}
+
+function fatorial(){
+  if(numeroFator.value === ''){
+    alert("Favor, insira algum número")
+  }else{
+    fatorDeUmNumero()
+  }
+}
+
+// eventos
+fator.addEventListener("click", fatorial )
